48ef264156d6b033d13d433c7db50c97eeb5cf92
amelieykw <amelieykw1991@gmail.com>
2016-12-13 22:32:46 +0100

    handle the function of the four operation buttons

--- app/src/main/java/com/yukaiwen/coolcalculator/CalcActivity.java
+++ app/src/main/java/com/yukaiwen/coolcalculator/CalcActivity.java
@@ -9,6 +9,19 @@ import android.widget.TextView;
 
 public class CalcActivity extends Activity {
 
+    String runningNumber = "";  // for display all the numbers entered in results view
+//  STEP 04 : handle the operations
+    String leftValueStr = "";
+    String rightValueStr = "";
+    public enum Operation {
+        ADD, SUBTRACT, MULTIPLY, DIVIDE, EQUAL
+    }
+    Operation currentOperation;
+    int result = 0; // the final result of operations
+
+//  Results View
+    TextView resultsView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,108 +46,111 @@ public class CalcActivity extends Activity {
         ImageButton addBtn = (ImageButton)findViewById(R.id.addBtn);
         ImageButton calcBtn = (ImageButton)findViewById(R.id.calcBtn);
 
-//      Results View
-        TextView resultsView = (TextView)findViewById(R.id.ResultsView);
-
 //      clear button
         Button clearBtn = (Button)findViewById(R.id.clearBtn);
+
+//      Results View
+        resultsView = (TextView)findViewById(R.id.ResultsView);
+
+//      STEP 01 : once the app loads, it sets the resultsView to empty
+        resultsView.setText("");
 
 //      set the actions
         zeroBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                numberPressed(0); // STEP 03 : press the number
             }
         });
 
         oneBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                numberPressed(1); // STEP 03 : press the number
             }
         });
 
         twoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                numberPressed(2); // STEP 03 : press the number
             }
         });
 
         threeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                numberPressed(3); // STEP 03 : press the number
             }
         });
 
         fourBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                numberPressed(4); // STEP 03 : press the number
             }
         });
 
         fiveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                numberPressed(5); // STEP 03 : press the number
             }
         });
 
         sixBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                numberPressed(6); // STEP 03 : press the number
             }
         });
 
         sevenBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                numberPressed(7); // STEP 03 : press the number
             }
         });
 
         eightBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                numberPressed(8); // STEP 03 : press the number
             }
         });
 
         nineBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                numberPressed(9); // STEP 03 : press the number
             }
         });
 
         divideBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                processOperation(Operation.DIVIDE);
             }
         });
 
         multiplyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                processOperation(Operation.MULTIPLY);
             }
         });
 
         subtractBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                processOperation(Operation.SUBTRACT);
             }
         });
 
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                processOperation(Operation.ADD);
             }
         });
 
@@ -151,13 +167,44 @@ public class CalcActivity extends Activity {
 
             }
         });
+    }
 
-        resultsView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+//  STEP 04 : handle the operations
+    void processOperation(Operation operation) {
+        if (currentOperation != null) {
+            if(runningNumber != "") {
+                rightValueStr = runningNumber;
+                runningNumber = "";
 
+                switch (currentOperation) {
+                    case ADD:
+                        result = Integer.parseInt(leftValueStr) + Integer.parseInt(rightValueStr);
+                        break;
+                    case SUBTRACT:
+                        result = Integer.parseInt(leftValueStr) - Integer.parseInt(rightValueStr);
+                        break;
+                    case MULTIPLY:
+                        result = Integer.parseInt(leftValueStr) * Integer.parseInt(rightValueStr);
+                        break;
+                    case DIVIDE:
+                        result = Integer.parseInt(leftValueStr) / Integer.parseInt(rightValueStr);
+                        break;
+                }
+
+                leftValueStr = String.valueOf(result);
+                resultsView.setText(leftValueStr);
             }
-        });
+        }else{
+//            this is the very first time that operators being pressed
+            leftValueStr = runningNumber;
+            runningNumber = "";
+        }
+        currentOperation = operation;
+    }
 
+//  STEP 02 : create a function for combining all the number pressed and then set the combination to results view
+    void numberPressed(int number) {
+        runningNumber += String.valueOf(number);
+        resultsView.setText(runningNumber);
     }
 }
